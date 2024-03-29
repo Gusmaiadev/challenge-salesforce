d6b3954f3c27e71c7086d01acc562d9097a22364
Gustavo Maia <144071835+Gusmaiadev@users.noreply.github.com>
2024-03-29 13:24:03 -0300

    correção texto produtos

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import Header from "./Header/page";
 import Footer from "./Footer/page";
 import Link from 'next/link';
+import icon from '../../public/favicon.ico'
 
 
 
@@ -20,6 +21,7 @@ export default function RootLayout({
     <html lang="pt-br">
       <head>
         <Link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"></Link>
+        <link rel="icon" href="public/favicon.ico" />
         </head>
       <body>
         <Header/>
